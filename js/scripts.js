d6d10634130fe6962a16305613b0cbd5b52ab395
Oona roe and Qudsia Ahmad <oona@>
2017-05-22 20:38:16 -0700

    add css, js, and img files to our basic jquery directory.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+jquery(document).ready(function() {
+  $(".clickable").click(function() {
+     $(".walrus-showing").toggle();
+     $(".walrus-hidden").toggle();
+   });
+});
+ jQuery("h1").click(function() {
+    alert("This is a header."();
+   });
+
+  jQuery("p").click(function() {
+
+    alert("This is a paragraph.");
+    $("img").show();
+   });
+
+  jQuery("img").click(function() {
+    alert("This is an image.");
+ });
+});
